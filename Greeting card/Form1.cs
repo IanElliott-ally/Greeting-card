1e96dde5b59e99a2b1c6862ed449bb581adffc06
Ian Elliott <ianelli199@ed.amdsb.ca>
2019-09-24 13:06:44 -0400

    Birthday Card For Godzilla

--- Greeting card/Form1.cs
+++ Greeting card/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
-
+using System.Media;
 namespace Greeting_card
 {
     public partial class Form1 : Form
@@ -26,55 +26,98 @@ namespace Greeting_card
             SolidBrush titlebrush = new SolidBrush(Color.Black);
             SolidBrush borderBrush = new SolidBrush(Color.DarkBlue);
             SolidBrush borderBrush1 = new SolidBrush(Color.Black);
+            SoundPlayer Stomp = new SoundPlayer(Properties.Resources.Grenade);
+            SoundPlayer Screee = new SoundPlayer(Properties.Resources.Godzilla);
 
             g.Clear(Color. Aqua);
-            g.DrawString("GREETINGS!", titlefont, titlebrush, 180, 60);
+            g.DrawString("GREETINGS!", titlefont, titlebrush, 205, 60);
+            Stomp.Play();
+            Thread.Sleep(1000);
+            Stomp.Play();
                 
             //frame1
-            g.DrawString("AND", titlefont, titlebrush, 440, 60);
-            Thread.Sleep(700);
+            g.DrawString("AND", titlefont, titlebrush, 465, 60);
+            Thread.Sleep(1000);
+            Stomp.Play();
             
             //frame 2
-            g.DrawString("HAPPY", titlefont, titlebrush, 200, 130);
-            Thread.Sleep(700);
+            g.DrawString("HAPPY", titlefont, titlebrush, 190, 130);
+            Thread.Sleep(1000);
+            Stomp.Play();
             
             //frame 3
-            g.DrawString("BIRTH", titlefont, titlebrush, 360, 130);
-            Thread.Sleep(700);
+            g.DrawString("BIRTH", titlefont, titlebrush, 350, 130);
+            Thread.Sleep(1000);
+            Stomp.Play();
             
             //frame 4
-            g.DrawString("DAY", titlefont, titlebrush, 480, 130);
-            Thread.Sleep(700);
+            g.DrawString("DAY", titlefont, titlebrush, 470, 130);
+            Thread.Sleep(1000);
+            Stomp.Play();
 
             //frame 5
-            g.DrawString("GODZILLA!!!", titlefont, titlebrush, 250, 190);
-            Thread.Sleep(700);
+            g.DrawString("GODZILLA!!!", titlefont, titlebrush, 250, 210);
+            Thread.Sleep(1000);
+            Screee.Play();
+            Thread.Sleep(6500);
 
-            //frame 6 
-            g.DrawString("HOPE", titlefont, titlebrush, 100, 330);
-            Thread.Sleep(700);
+            
+
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+            
+        { Graphics g = this.CreateGraphics();
+            Pen titlePen = new Pen(Color.Brown);
+            Font titlefont = new Font("Arial", 30);
+            SolidBrush titlebrush = new SolidBrush(Color.Black);
+            SolidBrush borderBrush = new SolidBrush(Color.DarkBlue);
+            SolidBrush borderBrush1 = new SolidBrush(Color.Black);
+            SoundPlayer Stomp = new SoundPlayer(Properties.Resources.Grenade);
+            SoundPlayer Screee = new SoundPlayer(Properties.Resources.Godzilla);
+
+            g.Clear(Color.Aqua);
+            //frame 6
+            g.DrawString("HOPE", titlefont, titlebrush, 100, 120);
+            Thread.Sleep(1000);
+            Stomp.Play();
 
             //frame 7
-            g.DrawString("YOU", titlefont, titlebrush, 230, 330);
-            Thread.Sleep(700);
+            g.DrawString("YOU", titlefont, titlebrush, 230, 120);
+            Thread.Sleep(1000);
+            Stomp.Play();
 
             //frame 8
-            g.DrawString("HAVE", titlefont, titlebrush, 340, 330);
-            Thread.Sleep(700);
+            g.DrawString("HAVE", titlefont, titlebrush, 340, 120);
+            Thread.Sleep(1000);
+            Stomp.Play();
 
             //frame 9
-            g.DrawString("AN", titlefont, titlebrush, 470, 330);
-            Thread.Sleep(700);
+            g.DrawString("AN", titlefont, titlebrush, 470, 120);
+            Thread.Sleep(1000);
+            Stomp.Play();
 
             //frame 10
-            g.DrawString("AMAZING", titlefont, titlebrush, 550, 330);
-            Thread.Sleep(700);
+            g.DrawString("AMAZING", titlefont, titlebrush, 550, 120);
+            Thread.Sleep(1000);
+            Stomp.Play();
 
             //frame 11
-            g.DrawString("65th!!!", titlefont, titlebrush,  350, 380);
-            Thread.Sleep(700);
+            g.DrawString("65th!!!", titlefont, titlebrush, 350, 180);
+            Thread.Sleep(1000);
 
+            //frame 12
+            g.DrawString("CHEERS", titlefont, titlebrush, 150, 290);
+            Thread.Sleep(1000);
 
+            //frame 13
+            g.DrawString("FROM", titlefont, titlebrush, 340, 290);
+            Thread.Sleep(1000);
+
+            //frame 14
+            g.DrawString("SSES", titlefont, titlebrush, 490, 290);
+            Thread.Sleep(1000);
         }
     }
 }
